e6b4ff123a86aab0ffcb5a674ac1fc603db0fce3
Valeria Velasquez <39226355+valeriavelasquez@users.noreply.github.com>
2020-10-10 17:41:02 -0400

    Add files via upload

--- uarray2.c
+++ uarray2.c
@@ -14,8 +14,12 @@ struct T {
     int size;
 };
 
+/*
+    Will malloc memory for the struct, client must call
+    UArray2_free to free memory allocated in the function
+*/
 T UArray2_new(int width, int height, int size) {
-    // assert(width >= 0 && height >= 0 && size >= 0);
+    assert(width >= 0 && height >= 0 && size >= 0);
     T new_struct; 
     NEW(new_struct);
     new_struct->width = width;
@@ -26,30 +30,33 @@ T UArray2_new(int width, int height, int size) {
 }
 
 int UArray2_width(T uarray2) {
-    assert(uarray2);
+    assert(uarray2 != NULL);
     return uarray2->width;
 }
 
 int UArray2_height(T uarray2) {
-    assert(uarray2);
+    assert(uarray2 != NULL);
     return uarray2->height;
 }
 
 int UArray2_size(T uarray2) {
-    assert(uarray2);
+    assert(uarray2 != NULL);
     return uarray2->size;
 }
 
-void *UArray2_at(T uarray2, int row, int col) {
-    assert(uarray2);
-    //assert for row and col bounds
-    if (uarray2->width > uarray2->height) {
-        return UArray_at(uarray2->array, (uarray2->width)*col+row);
-    }
+void *UArray2_at(T uarray2, int col, int row) {
+    assert(uarray2 != NULL);
+    assert(row < uarray2->height && col < uarray2->width);
     return UArray_at(uarray2->array, (uarray2->width)*row+col);
 }
 
-void UArray2_map_row_major(T uarray2, void apply(int col, int row, T uarray2, void *elem, void *cl), void *cl) {
+/*
+    For each row, this function will loop through each column 
+    before indexing to the next row
+*/
+void UArray2_map_row_major(T uarray2, void apply(int col, 
+        int row, T uarray2, void *elem, void *cl), void *cl) {
+    assert(uarray2 != NULL);
     for (int r = 0; r < uarray2->height; r++) {
         for (int c = 0; c < uarray2->width; c++) {
             apply(c, r, uarray2, UArray2_at(uarray2, c, r), cl);
@@ -57,7 +64,13 @@ void UArray2_map_row_major(T uarray2, void apply(int col, int row, T uarray2, vo
     }
 }
 
-void UArray2_map_col_major(T uarray2, void apply(int col, int row, T uarray2, void *elem, void *cl), void *cl) {
+/*
+    For each column, this function will loop through each row 
+    before indexing to the next column
+*/
+void UArray2_map_col_major(T uarray2, void apply(int col, 
+        int row, T uarray2, void *elem, void *cl), void *cl) {
+    assert(uarray2 != NULL);
     for (int c = 0; c < uarray2->width; c++) {
         for (int r = 0; r < uarray2->height; r++) {
             apply(c, r, uarray2, UArray2_at(uarray2, c, r), cl);
